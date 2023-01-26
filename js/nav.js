35baf7c5401290b6f3327b9875a3326b364f2678
gero99 <gerolagarrigue9@gmail.com>
2023-01-26 12:09:09 -0300

    update

--- js/nav.js
+++
@@ -1,9 +0,0 @@
-var header = document.getElementsByClassName('header')
-window.addEventListener('scroll', () => {
-  var scroll = window.scrollY;
-  if (scroll > 10) {
-    header.style.backgroundColor = '#0000';
-  } else {
-    header.style.backgroundColor = 'transparent';
-  }
-})
