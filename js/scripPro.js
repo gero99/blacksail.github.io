2d1c958d586657ab415193e66f52b917095a8c62
gero99 <gerolagarrigue9@gmail.com>
2023-01-28 11:27:50 -0300

    update

--- js/scripPro.js
+++ js/scripPro.js
@@ -1,5 +1,5 @@
-let menuBtn = document.querySelector('#menu-btn');
-let navbar = document.querySelector('.header .flex .navbar');
+var menuBtn = document.querySelector('#menu-btn');
+var navbar = document.querySelector('.header .flex .navbar');
 
 menuBtn.onclick = () =>{
    menuBtn.classList.toggle('fa-times');
@@ -10,66 +10,3 @@ window.onscroll = () =>{
    menuBtn.classList.remove('fa-times');
    navbar.classList.remove('active');
 }
-
-var swiper = new Swiper(".course-slider", {
-   spaceBetween: 20,
-   grabCursor:true,
-   loop:true,
-   pagination: {
-     el: ".swiper-pagination",
-     clickable: true,
-   },
-   breakpoints: {
-      540: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      1024: {
-        slidesPerView: 3,
-      },
-   },
-});
-
-var swiper = new Swiper(".teachers-slider", {
-   spaceBetween: 20,
-   grabCursor:true,
-   loop:true,
-   pagination: {
-     el: ".swiper-pagination",
-     clickable: true,
-   },
-   breakpoints: {
-      540: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      1024: {
-        slidesPerView: 3,
-      },
-   },
-});
-
-var swiper = new Swiper(".reviews-slider", {
-   spaceBetween: 20,
-   grabCursor:true,
-   loop:true,
-   pagination: {
-     el: ".swiper-pagination",
-     clickable: true,
-   },
-   breakpoints: {
-      540: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      1024: {
-        slidesPerView: 3,
-      },
-   },
-});
