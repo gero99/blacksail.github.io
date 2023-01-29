2792aa07db60116c6689945fd4116f8e7ad58617
gero99 <gerolagarrigue9@gmail.com>
2023-01-29 14:21:33 -0300

    update

---
+++ js/multiIdiomaEnH.js
@@ -0,0 +1,10 @@
+var check = document.querySelector(".checkBox")
+check.addEventListener('click', idioma);
+function idioma () {
+    let id = check.checked;
+    if(id == true) {
+        location.href="paginasEN/historiaEN.html"
+    } else{
+        location.href="../historia.html"
+    }
+}
---
+++ js/multiIdiomaEnQs.js
@@ -0,0 +1,10 @@
+var check = document.querySelector(".checkBox")
+check.addEventListener('click', idioma);
+function idioma () {
+    let id = check.checked;
+    if(id == true) {
+        location.href="./paginasEN/quienesomosEN.html"
+    } else{
+        location.href="../quienesomos.html"
+    }
+}
